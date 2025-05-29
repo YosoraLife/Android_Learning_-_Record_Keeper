3a6562a621a8a232326b5f65ff63adf2b58d548e
Remco <remco@yosora.com>
2025-05-29 17:28:24 +0200

    Added EditRunningRecordActivity + setOnClickListeners

--- app/src/main/java/com/yosora/recordkeeper/RunningFragment.kt
+++ app/src/main/java/com/yosora/recordkeeper/RunningFragment.kt
@@ -1,19 +1,42 @@
 package com.yosora.recordkeeper
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.yosora.recordkeeper.databinding.FragmentRunningBinding
 
 class RunningFragment : Fragment() {
+
+    private lateinit var binding: FragmentRunningBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_running, container, false)
+    ): View {
+        binding = FragmentRunningBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupClickListeners()
+
+    }
+
+    private fun setupClickListeners() {
+        binding.container5km.setOnClickListener { launchRunningRecordScreen() }
+        binding.container10km.setOnClickListener { launchRunningRecordScreen() }
+        binding.containerHalfMarathon.setOnClickListener { launchRunningRecordScreen() }
+        binding.containerMarathon.setOnClickListener { launchRunningRecordScreen() }
+    }
+
+    private fun launchRunningRecordScreen() {
+        val intent = Intent(context, EditRunningRecordActivity::class.java)
+        startActivity(intent)
     }
 
 }
